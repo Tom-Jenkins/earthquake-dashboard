758d1fbc54146bc13e99d18e1dc05c5769694b9c
Jenkins <t.l.jenkins@exeter.ac.uk>
2025-03-26 15:01:30 +0000

    minor update

--- js/main.js
+++ js/main.js
@@ -105,7 +105,7 @@ if (viewer) {
                         window.open(url, "_blank", "noopener,noreferrer"); 
                     });
                 });
-            }, 100); // short delay to ensure InfoBox content loads
+            }, 500); // short delay to ensure InfoBox content loads
         }
     });
 };
