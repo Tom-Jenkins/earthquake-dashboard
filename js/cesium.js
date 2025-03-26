f852fe265b61d5314f8f131a12b1b7cc968ea38a
Jenkins <t.l.jenkins@exeter.ac.uk>
2025-03-26 11:38:01 +0000

    test new usgs popup link on mobiles

--- js/cesium.js
+++ js/cesium.js
@@ -66,7 +66,7 @@ export const entityPopupContent = function (mag, depth, lon, lat, time, place, u
         <div>
             <p>Magnitude: ${mag}</p>
             <p>Depth: ${depth.toFixed(0)} km</p>
-            <p>Origin: <a href="#" class="usgs-link" data-url="${usgsUrl}" style="color: #aef; text-decoration: none;">${place}&nbsp;
+            <p>Origin: <a class="usgs-link" data-url="${usgsUrl}" style="color: #aef; text-decoration: none; cursor: pointer;">${place}&nbsp;
                     <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
                         <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
